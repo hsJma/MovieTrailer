3e7345920f90fb7630c8ecf6076b4cfbb92a6e6b
hsJma <bright.moe@gmail.com>
2015-11-03 15:41:42 +0800

    add comments

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 import webbrowser
 
 class Movie():
+	""" Base class to store movie information. 
+
+	Attributes:
+		title: A string
+		storyline: A string description of movie
+		poster_image_url: A string 
+		trailer_youtube_url: A string
+	"""
 	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.storyline = movie_storyline
